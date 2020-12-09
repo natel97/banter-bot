1919ced3b1a48c0c0f88f08c3e6bd557c38324d2
Nathanial Lubitz <nate.lubitz@gmail.com>
2020-12-08 22:46:37 -0600

    remove unnecessary code; better readme; fix drone secret typo

--- readme.md
+++ readme.md
@@ -1,1 +1,43 @@
+# Banter Bot
+
 [![Build Status](https://gh-drone.nathaniallubitz.com/api/badges/natel97/banter-bot/status.svg)](https://gh-drone.nathaniallubitz.com/natel97/banter-bot)
+
+## Description
+
+Banter Bot is a bot with the intent of encouraging discussions throughout a Discord server. It is hosted with Kubernetes in an AtomicPI & Asus Tinkerboard and deployed via Drone.
+
+## What it does now
+
+It's very basic. I wasn't even planning on posting this on GitHub. But I figured it might be nice to have available online? Who knows!
+
+### Generic
+
+- Plays a fun sound when someone joins a voice channel
+
+### Commands
+
+Commands are triggered when the bot is @'d if the text contains the following:
+
+#### Poll us
+
+This will ask a random question, possibly tag someone, and add a 👍 and 👎 to allow people to voice their opinion.
+
+Ex: Can @Joe tell good jokes?
+
+👍 👎
+
+#### Provoke our thoughts
+
+Asks open ended questions that may tag someone to make it more personal and fun.
+
+Ex: If @Josh were a sandwich, which sandwich would they be?
+
+## Ideas to vet for the future
+
+- Auto posting in a channel if 2 people are in a room (encourage more to join)
+- Randomly asking people questions
+- Integrating games like hangman or starting up a round of Among Us
+- Birthday announcements
+- Video streams?
+- DM conversations / EMacs Doctor
+- Random chat sessions (bot pics two random people and DM responses get sent to mystery person until tbd)
--- src/index.js
+++ src/index.js
@@ -1,53 +1,38 @@
-const { generateRandomTime, waitUntilTime } = require('./timeUtils')
-
-function waitAndRecurse(incrementDay = false) {
-    const time = generateRandomTime({ hour: 8, minute: 0 }, { hour: 23, minute: 0 }, incrementDay)
-    waitUntilTime(time).then(() => {
-        console.log("time is now")
-        waitAndRecurse(true)
-    })
-}
-
 (() => {
-    require("dotenv").config();
-    const Discord = require("discord.js");
-    const client = new Discord.Client();
-    client.login(process.env.TOKEN)
-    client.once('ready', () => {
-        client.on('voiceStateUpdate', async (before, after) => {
-            if (before.member.id === client.user.id) return;
-            console.log({ before: before.channelID, after: after.channelID })
-            if (before.channelID === null && after.channelID) {
-                console.log("trying to join")
-                // Play startup noise :)
-                const connection = await after.channel.join();
-                console.log("joined")
-                const dispatch = connection.play("new-connection.mp3");
-                dispatch.on("start", () => console.log('starting'))
-                dispatch.on('finish', () => connection.disconnect())
-                dispatch.on('error', (e) => { connection.disconnect(); console.log(e) })
-            }
-        })
+  require("dotenv").config();
+  const Discord = require("discord.js");
+  const client = new Discord.Client();
+  client.login(process.env.TOKEN);
+  client.once("ready", () => {
+    client.on("voiceStateUpdate", async (before, after) => {
+      if (before.member.id === client.user.id) return;
+      if (before.channelID === null && after.channelID) {
+        // Play startup noise :)
+        const connection = await after.channel.join();
+        const dispatch = connection.play("new-connection.mp3");
+        dispatch.on("finish", () => connection.disconnect());
+        dispatch.on("error", () => connection.disconnect());
+      }
+    });
 
-        client.on('messageReactionAdd', (react, user) => {
-            if (react.message.author.id !== client.user.id) return;
-            // react.message.channel.send(`I saw what you did <@${user.id}> ${react.emoji}`);
-        })
+    client.on("messageReactionAdd", (react, user) => {
+      if (react.message.author.id !== client.user.id) return;
+      // react.message.channel.send(`I saw what you did <@${user.id}> ${react.emoji}`);
+    });
 
-        client.on('message', async msg => {
-            if (msg.author.bot) return
-            if (!msg.mentions.has({ id: client.user.id })) return
-            console.log(msg.content)
-            if (msg.content.toLowerCase().includes("provoke our thoughts")) {
-                const randomQuestion = await require("./questions").getRandomQuestion(msg);
-                return msg.channel.send(randomQuestion)
-            } if (msg.content.toLowerCase().includes("poll us")) {
-                return await require("./questions").createRandomPoll(msg)
-            }
-            msg.reply("Please just let me exist in peace.")
-        })
-    })
-
-    waitAndRecurse()
-})()
-
+    client.on("message", async (msg) => {
+      if (msg.author.bot) return;
+      if (!msg.mentions.has({ id: client.user.id })) return;
+      if (msg.content.toLowerCase().includes("provoke our thoughts")) {
+        const randomQuestion = await require("./questions").getRandomQuestion(
+          msg
+        );
+        return msg.channel.send(randomQuestion);
+      }
+      if (msg.content.toLowerCase().includes("poll us")) {
+        return await require("./questions").createRandomPoll(msg);
+      }
+      msg.reply("Please just let me exist in peace.");
+    });
+  });
+})();
